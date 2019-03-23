df304ea7763d2a16ccfeac7dfe56ee1f355bb2cf
GeorgeGithiri5 <46107866+GeorgeGithiri5@users.noreply.github.com>
2019-03-23 17:52:17 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<title>File System</title>
+
+			<meta charset="utf-8">
+			<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+			<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+			<link rel="stylesheet" type="text/css" href="https://font.googleapis.com?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i,">
+			<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+			<link rel="stylesheet" type="text/css" href="css/style.css">
+			<script src="js/validate"></script>			
+			<script src="js/jquery-1.11.3.min.js"></script>
+			<script src="js/bootsrtap.min.js"></script>
+			<style type="tect/css">
+				.navbar(
+				margin-top: 20px;)
+			</style>
+	</head>
